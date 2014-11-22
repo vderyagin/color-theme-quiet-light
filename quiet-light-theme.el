73179a3498e961bc8eb5e577bb86d1449f9d0c22
Victor Deryagin <vderyagin@gmail.com>
2014-11-22 20:21:41 +0200

    ledger: face for periodic transactions.

--- quiet-light-theme.el
+++ quiet-light-theme.el
@@ -406,6 +406,7 @@
    `(ledger-font-payee-cleared-face ((,class (:inherit font-lock-string-face :weight bold))))
    `(ledger-font-payee-pending-face ((,class (:inherit warning))))
    `(ledger-font-payee-uncleared-face ((,class (:inherit ledger-font-payee-pending-face :weight normal))))
+   `(ledger-font-periodic-xact-face ((,class (:inherit font-lock-string-face))))
    `(ledger-font-posting-account-face ((,class (:inherit font-lock-variable-name-face))))
    `(ledger-font-posting-amount-face ((,class (:inherit font-lock-builtin-face :weight normal))))
    `(ledger-font-posting-date-face ((,class (:inherit bold))))
